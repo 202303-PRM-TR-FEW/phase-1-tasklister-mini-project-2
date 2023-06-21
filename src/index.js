4f9ded7e4666c2af351ce72d3ea587756658a959
haroon <haroon.seena2017@gmail.com>
2023-06-21 17:48:22 +0300

    iam tired i need some sleep then ill finish it today

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const input = document.getElementById("new-task-description");
+const btn = document.getElementById("btn");
+const ul = document.getElementById("tasks");
+const li = document.createElement("li");
+
+btn.addEventListener("click",function(e){
+e.preventDefault()
+const li = document.createElement("li");
+li.innerHTML = input.value;
+ul.append(li);
+})
